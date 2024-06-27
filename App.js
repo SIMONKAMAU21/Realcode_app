2310a7e14a4c7ac61b83367e33b7bf8ea05c5a52
simon kamau <simogatuma21@gmail.com>
2024-06-27 12:49:40 +0300

    modified the app cpmponent

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { ActivityIndicator, StyleSheet, Dimensions } from "react-native";
-import Domain from "./pages/Domain";
-import Login from "./pages/Login";
-import Accounts from "./pages/account";
-import Settings from "./pages/Settings";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
-import { QueryClient, QueryClientProvider } from "react-query";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ActivityIndicator, StyleSheet, StatusBar } from 'react-native';
+import Domain from './pages/Domain';
+import Login from './pages/Login';
+import Accounts from './pages/account';
+import Settings from './pages/Settings';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Updates from 'expo-updates';
-import Icon from "react-native-vector-icons/Ionicons";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -23,17 +23,17 @@ const HomeTabs = () => {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ color, size }) => {
           let iconName;
-          if (route.name === "Accounts") {
-            iconName = "home";
-          } else if (route.name === "info") {
-            iconName = "information-circle";
+          if (route.name === 'Accounts') {
+            iconName = 'home';
+          } else if (route.name === 'info') {
+            iconName = 'information-circle';
           }
           return <Icon name={iconName} size={size} color={color} />;
         },
       })}
     >
-      <Tab.Screen name="Accounts" component={Accounts} />
-      <Tab.Screen name="info" component={Settings} />
+      <Tab.Screen name='Accounts' component={Accounts} />
+      <Tab.Screen name='info' component={Settings} />
     </Tab.Navigator>
   );
 };
@@ -41,21 +41,20 @@ const HomeTabs = () => {
 const App = () => {
   const [initialRoute, setInitialRoute] = useState(null);
   const [loading, setLoading] = useState(true);
-  const { width, height } = Dimensions.get("window");
 
   useEffect(() => {
     const checkDomain = async () => {
       try {
-        const storedDomain = await AsyncStorage.getItem("userdomain");
-        console.log("Stored domain:", storedDomain);
+        const storedDomain = await AsyncStorage.getItem('userdomain');
+        console.log('Stored domain:', storedDomain);
         if (storedDomain) {
-          setInitialRoute("Login");
+          setInitialRoute('Login');
         } else {
-          setInitialRoute("Domain");
+          setInitialRoute('Domain');
         }
       } catch (error) {
-        console.error("Failed to retrieve domain", error);
-        setInitialRoute("Domain");
+        console.error('Failed to retrieve domain', error);
+        setInitialRoute('Domain');
       } finally {
         setLoading(false);
       }
@@ -74,7 +73,7 @@ const App = () => {
           }
         }
       } catch (error) {
-        console.error("Error checking for updates", error);
+        console.error('Error checking for updates', error);
       }
     };
 
@@ -85,34 +84,23 @@ const App = () => {
     ...DefaultTheme,
     colors: {
       ...DefaultTheme.colors,
-      primary: "aliceblue",
+      primary: 'aliceblue',
     },
   };
 
   if (loading) {
-    return <ActivityIndicator size="large" color="#0077b6" style={styles.container} />;
+    return <ActivityIndicator size='large' color='#0077b6' style={styles.container} />;
   }
 
   return (
     <QueryClientProvider client={queryClient}>
       <PaperProvider theme={theme}>
         <NavigationContainer>
+          <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
           <Stack.Navigator initialRouteName={initialRoute}>
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="Domain"
-              component={Domain}
-            />
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="Login"
-              component={Login}
-            />
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="Home"
-              component={HomeTabs}
-            />
+            <Stack.Screen options={{ headerShown: false }} name='Domain' component={Domain} />
+            <Stack.Screen options={{ headerShown: false }} name='Login' component={Login} />
+            <Stack.Screen options={{ headerShown: false }} name='Home' component={HomeTabs} />
           </Stack.Navigator>
         </NavigationContainer>
       </PaperProvider>
@@ -123,8 +111,8 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
